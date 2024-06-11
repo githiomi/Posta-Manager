b417d72b60500e26eb700cbac056eedb2f3748b9
Githiomi Daniel <dangit@mcb.mu>
2024-06-11 10:30:15 +0400

    Added all required libraries - material, tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from "@angular/material/divider";
+import { NgOptimizedImage } from "@angular/common";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [RouterOutlet, MatSidenavModule, MatListModule, MatDividerModule, NgOptimizedImage, MatButtonModule],
+  templateUrl: 'app.component.html',
   styles: [],
 })
 export class AppComponent {
-  title = 'angular_blog_post';
+  title = 'Angular Blog Post';
+
+  opened = signal<boolean>(false);
+
+  toggleSideBar = (): void => this.opened.update(prev => !prev);
+
+  list: string[] = ["Home", "About", "Contact"]
 }
